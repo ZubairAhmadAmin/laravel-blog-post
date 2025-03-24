5fcdcc745bb12fcc26df5a2d37355c57eea08091
Zubair Amin <aminzubair2021@gmail.com>
2025-03-24 12:20:21 +0430

    change from second user

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     // return view('welcome');
 
-    return 'Hello';
+    // return 'Hello';
+
+    return 'change from second user';
 });
